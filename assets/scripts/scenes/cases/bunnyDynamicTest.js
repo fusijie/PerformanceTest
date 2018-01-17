424c0fc0d0339df22ea1623e4dfdf689f4712634
fusijie <fusijie@vip.qq.com>
2018-01-17 16:24:55 +0800

    add bunny dynamic test case

---
+++ assets/scripts/scenes/cases/bunnyDynamicTest.js
@@ -0,0 +1,153 @@
+const baseScene = require("baseScene");
+const config = require("config");
+
+let bunnyType = 0;
+let gravity = 0;
+
+let maxX = 0;
+let minX = 0;
+let maxY = 0;
+let minY = 0;
+
+let totalCount = 0;
+let count = 0;
+let amount = 0;
+let dynamicCount = 0;
+
+cc.Class({
+    extends: baseScene,
+
+    properties: {
+        tex: cc.Texture2D,
+        number: cc.Label
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this._super(this.name.match(/<(\S*)>/)[1]);
+
+        this.number.node.zIndex = config.HIGHEST_ZINDEX;
+
+        this.reset();
+    },
+
+    reset: function () {
+        this.bunnys = [];
+        this.inited = false;
+
+        this.bunnyFrames = [];
+        this.bunnyFrames.push(new cc.SpriteFrame(this.tex, cc.rect(2, 47, 26, 37)));
+        this.bunnyFrames.push(new cc.SpriteFrame(this.tex, cc.rect(2, 86, 26, 37)));
+        this.bunnyFrames.push(new cc.SpriteFrame(this.tex, cc.rect(2, 125, 26, 37)));
+        this.bunnyFrames.push(new cc.SpriteFrame(this.tex, cc.rect(2, 164, 26, 37)));
+        this.bunnyFrames.push(new cc.SpriteFrame(this.tex, cc.rect(2, 2, 26, 37)));
+        this.currentFrame = this.bunnyFrames[0];
+
+        bunnyType = 0;
+        gravity = 0.5;
+
+        maxX = cc.winSize.width / 2;
+        maxY = cc.winSize.height / 2;
+        minX = -maxX;
+        minY = -maxY;
+
+        totalCount = 2500;
+        count = 0;
+        amount = 50;
+        dynamicCount = 100;
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function (dt) {
+        let bunny, bunnysp, i;
+        if (this.bunnys.length < totalCount && !this.inited) {
+            for (i = 0; i < amount; i++) {
+                bunny = new cc.Node();
+                bunnysp = bunny.addComponent(cc.Sprite);
+                bunnysp.spriteFrame = this.currentFrame;
+                bunny.speedX = Math.random() > 0.5 ? Math.random() * 10 : - Math.random() * 10;
+                bunny.speedY = (Math.random() * 10) - 5;
+                bunny.x = 0;
+                bunny.y = 0;
+                bunny.anchorY = 1;
+                //bunny.alpha = 0.3 + Math.random() * 0.7;
+                this.bunnys.push(bunny);
+                bunny.scale = 0.5 + Math.random() * 0.5;
+                bunny.rotation = 360 * (Math.random() * 0.2 - 0.1);
+
+                this.node.addChild(bunny);
+                count++;
+            }
+            this.number.string = count;
+            bunnyType++;
+            bunnyType %= 5;
+            this.currentFrame = this.bunnyFrames[bunnyType];
+            if (this.bunnys.length >= totalCount) {
+                this.inited = true;
+            }
+        }
+
+        if (this.inited) {
+            i = this.bunnys.length;
+            while (i--) {
+                if (i < dynamicCount) {
+                    bunny = this.bunnys[i];
+                    bunny.removeFromParent();
+                    this.bunnys.splice(i, 1);
+                    count--;
+                }
+            }
+            for (i = 0; i < dynamicCount; i++) {
+                bunny = new cc.Node();
+                bunnysp = bunny.addComponent(cc.Sprite);
+                bunnysp.spriteFrame = this.currentFrame;
+                bunny.speedX = Math.random() > 0.5 ? Math.random() * 10 : - Math.random() * 10;
+                bunny.speedY = (Math.random() * 10) - 5;
+                bunny.x = 0;
+                bunny.y = 0;
+                bunny.anchorY = 1;
+                //bunny.alpha = 0.3 + Math.random() * 0.7;
+                this.bunnys.push(bunny);
+                bunny.scale = 0.5 + Math.random() * 0.5;
+                bunny.rotation = 360 * (Math.random() * 0.2 - 0.1);
+
+                this.node.addChild(bunny);
+                count++;
+            }
+            this.number.string = count;
+            bunnyType++;
+            bunnyType %= 5;
+            this.currentFrame = this.bunnyFrames[bunnyType];
+        }
+
+        for (i = 0; i < this.bunnys.length; i++) {
+            bunny = this.bunnys[i];
+
+            let x = bunny.x + bunny.speedX;
+            let y = bunny.y - bunny.speedY;
+            bunny.speedY += gravity;
+
+            if (x > maxX) {
+                bunny.speedX *= -1;
+                x = maxX;
+            }
+            else if (x < minX) {
+                bunny.speedX *= -1;
+                x = minX;
+            }
+
+            if (y < minY) {
+                bunny.speedY *= -0.85;
+                y = minY;
+                if (Math.random() > 0.5) {
+                    bunny.speedY -= Math.random() * 6;
+                }
+            }
+            else if (y > maxY) {
+                bunny.speedY = 0;
+                y = maxY;
+            }
+            bunny.setPosition(x, y);
+        }
+    },
+});
--- assets/scripts/utils/config.js
+++ assets/scripts/utils/config.js
@@ -1,12 +1,57 @@
 let config =  {};
 
+/**
+ * name: test case name.
+ * scene: test case scene name.
+ * args: global scene args, you can use it in the scene component.
+ * platform: the specified platform, default all platforms.
+ * auto: it's auto test or not.
+ * desc: test case description.
+ */
 config.TEST_CASE = [
-    { name: "Bunny Test", scene: "bunnyTest", auto: false },
-    { name: "Bunny Test(iPhone 6, Safari, 2600)", scene: "bunnyTest1", args: { count: 2600 }, auto: true },
-    { name: "Bunny Test(Vivo Y66, Default, 1500)", scene: "bunnyTest1", args: { count:  1500 }, auto: true },
-    { name: "Prefab Bunny Test", scene: "prefabBunnyTest", auto: false},
-    { name: "Prefab Bunny Test(iPhone 6, Safari, 2600)", scene: "prefabBunnyTest1", args: { count: 2600 }, auto: true },
-    { name: "Prefab Bunny Test(Vivo Y66, Default, 1500)", scene: "prefabBunnyTest1", args: { count: 1500 }, auto: true },
+    { 
+        name: "Bunny Test", 
+        scene: "bunnyTest", 
+        auto: false, 
+    },
+    { 
+        name: "Bunny Test(iPhone 6, Safari, 2600)", 
+        scene: "bunnyFixedCountTest", 
+        args: { count: 2600 }, 
+        platform: cc.sys.OS_IOS, 
+        auto: true, 
+    },
+    { 
+        name: "Bunny Test(Vivo Y66, Default, 1500)", 
+        scene: "bunnyFixedCountTest", 
+        args: { count:  1500 }, 
+        platform: cc.sys.OS_ANDROID, 
+        auto: true, 
+    },
+    { 
+        name: "Prefab Bunny Test", 
+        scene: "prefabBunnyTest", 
+        auto: false,
+    },
+    { 
+        name: "Prefab Bunny Test(iPhone 6, Safari, 2600)", 
+        scene: "prefabBunnyFixedCountTest", 
+        args: { count: 2600 }, 
+        platform: cc.sys.OS_IOS, 
+        auto: true, 
+    },
+    { 
+        name: "Prefab Bunny Test(Vivo Y66, Default, 1500)", 
+        scene: "prefabBunnyFixedCountTest", 
+        args: { count: 1500 }, 
+        platform: cc.sys.OS_ANDROID,
+        auto: true, 
+    },
+    {
+        name: "Bunny Dynamic Test",
+        scene: "bunnyDynamicTest",
+        auto: true,
+    },
 ];
 
 config.CURRENT_CASE = -1;
