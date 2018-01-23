2a9c382bdfce0b7d5bdedb82bf7173e6cdde49f2
fusijie <fusijie@vip.qq.com>
2018-01-23 14:00:31 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,91 +1,62 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let cors = require("cors");
+let mysql = require("mysql");
 
-let data = [
-	{
-		platform: 'OS X',
-		time: 1516264431454,
-		data:
-			[
-				null,
-				{
-					name: 'Bunny Test(iPhone 6, Safari, 2600)',
-					maxTime: 14,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Bunny Test(Vivo Y66, Chrome, 1500)',
-					maxTime: 8,
-					minTime: 0,
-					avgFps: '60'
-				},
-				null,
-				{
-					name: 'Prefab Bunny Test(iPhone 6, Safari, 2600)',
-					maxTime: 9,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Prefab Bunny Test(Vivo Y66, Chrome, 1500)',
-					maxTime: 8,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Bunny Add/Remove Test',
-					maxTime: 44,
-					minTime: 0,
-					avgFps: '59'
-				},
-				{ name: 'Bunny Tree Test', maxTime: 19, minTime: 0, avgFps: '60' },
-				{
-					name: 'Bunny Active/Inactive Test',
-					maxTime: 18,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Bunny Transform Animation Test',
-					maxTime: 13,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Bunny Frame Animation Test',
-					maxTime: 8,
-					minTime: 0,
-					avgFps: '60'
-				},
-				{
-					name: 'Bunny Transform Action Test',
-					maxTime: 14,
-					minTime: 0,
-					avgFps: '60'
-				}]
-	}
-];
+let data = [];
+let mysql_opt = {
+	host: '127.0.0.1',
+	user: 'root',
+	password: '',
+	database: 'PerformanceTest'
+};
 
-let app = express();
-app.use(cors());
-app.options("*", cors());
+// load data from mysql.
+function load_data() {
+	let connection = mysql.createConnection(mysql_opt);
+	connection.connect();
+	let sql_cmd = 'select * from `CreatorTestData`';
+	connection.query(sql_cmd, function (err, rows, fields) {
+		if (err) {
+			throw err;
+		}
+		for (let i = 0; i < rows.length; i++) {
+			data.push(JSON.parse(rows[i].data));
+		}
+	});
+	connection.end();
+}
 
-// create application/json parser
-let jsonParser = bodyParser.json();
+// start http server.
+function start_server() {
+	let app = express();
+	app.use(cors());
+	app.options("*", cors());
+	let jsonParser = bodyParser.json();
 
-app.post("/upload_result", jsonParser, function (req, res) {
-	if (!req.body) {
-		return res.sendStatus(400);
-	}
-	console.log(req.body);
-	data.push(req.body);
-	res.send(req.body);
-});
+	app.post("/upload_result", jsonParser, function (req, res) {
+		if (!req.body) {
+			return res.sendStatus(400);
+		}
+		let connection = mysql.createConnection(mysql_opt);
+		connection.connect();
+		let sql_cmd = "insert into CreatorTestData(data) values(?)"
+		connection.query(sql_cmd, JSON.stringify(req.body), function (err, rows, fields) {
+			if (err) {
+				throw err;
+			}
+		});
+		connection.end();
+		data.push(req.body);
+		res.send(req.body);
+	});
 
-app.post("/get_result", jsonParser, function (req, res) {
-	res.send(data);
-});
+	app.post("/get_result", jsonParser, function (req, res) {
+		res.send(data);
+	});
 
-app.listen(30000, () => console.log("Example app listening on port 30000!"));
+	app.listen(30000, () => console.log("Example app listening on port 30000!"));
+}
+
+load_data();
+start_server();
